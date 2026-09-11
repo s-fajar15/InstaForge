2e8b61fe2ad03bb02f9e5635eb4c85ea65e87263
KYJAR <fajarstok15@gmail.com>
2026-09-11 04:00:44 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+==========================================
+       Instagram Account Creator
+==========================================
+author     : S.fajar
+Versi       : 2.0
+Dibuat      : 2026
+License     : Proprietary (No redistribution or commercial use)
+==========================================
+"""
+from menu.main import MainMenu
+
+if __name__ == "__main__":
+    MainMenu()
